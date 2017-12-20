d407647cda12b6592b423c5f77b1f02ea3d4aee6
rcky <ruefe@rcky.ch>
2017-12-20 16:49:27 +0100

    feat(remote-microservice): adds the remote microservice component

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 const error = require('./src/error');
 
-module.export = function(app, options){
+const RemoteMicroservice = require('./src/RemoteMicroservice');
+
+const ServiceDiscoveryApi = require('./src/ServiceDiscoveryApi');
+const ServiceDiscovery = require('./src/ServiceDiscovery');
+
+module.exports = function(app, options){
+
+    const discovery = new ServiceDiscovery(options.services);
+    const component = new RemoteMicroservice(app, discovery, options);
+
+    if(options.api && options.api.disabled !== true){
+        const serviceDiscoveryApi = new ServiceDiscoveryApi(app, options.api);
+        RemoteMicroservice.mount(app, serviceDiscoveryApi, options.api);
+    }
+
+    app.set('remote-microservice', component);
 };
 
-module.export.error = error;
+module.exports.error = error;
