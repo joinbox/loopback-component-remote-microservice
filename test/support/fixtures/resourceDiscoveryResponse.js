8e4f330b0bc6017a8d603eff7f433a97ce161e8c
rcky <ruefe@rcky.ch>
2017-12-20 16:39:53 +0100

    test: update the test environment

--- test/support/fixtures/resourceDiscoveryResponse.js
+++ test/support/fixtures/resourceDiscoveryResponse.js
@@ -1,9 +1,13 @@
 // @note: the data is wrapped into a function to avoid modification of the base data (side-effects)
-module.exports = function(){
-    return [
+module.exports = function(exclude = []){
+    const response = [
         {
             name: 'Book',
             path: '/books',
+        },
+        {
+            name: 'Page',
+            path: '/pages',
         },
         {
             name: 'Author',
@@ -14,4 +18,5 @@ module.exports = function(){
             path: '/publishers',
         },
     ];
+    return response.filter((entry) => exclude.indexOf(entry.name) === -1);
 };
